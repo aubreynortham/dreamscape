9d1323708e0efcddea3aed975886f38a8d133fd0
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-08-10 17:45:02 -0400

    got this user devise issue ironed out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!, :except => [:welcome, :show, :get_explore, :get_collective]
-
+  before_action :authenticate_user!, :except => [:show, :get_explore, :get_collective]
 
 end
